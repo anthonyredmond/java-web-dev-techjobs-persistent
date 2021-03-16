c37625c697feac5a0d9976df4652b8b627ed9759
anthonyredmond <kushkurosaki@icloud.com>
2021-03-16 18:41:46 -0500

    DATA LAYER

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+ @Entity
 public class Employer extends AbstractEntity {
+    @NotBlank (message="location require")
+private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+     public Employer() {}
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,17 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank (message="skill description")
+    @Size (min = 4, max = 50)
+    private String description;
+
+
+    public Skill() {}
 
 }
